f1dbc362dc487da514c38d9447f824bde4c932e6
unknown <tejaskjain2003@gmail.com>
2023-11-12 20:59:59 +0530

    Event Management

--- server/app.js
+++ server/app.js
@@ -1,65 +1,63 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-require("dotenv").config();
-const eventRoutes = require("./routes/eventRoutes");
-const attendeeRoutes = require("./routes/attendeeRoutes");
-const authRoutes = require("./routes/authRoutes");
-const ticketRoutes = require("./routes/ticketRoutes");
-const reviewRoutes = require("./routes/reviewRoutes");
-const categoryRoutes=require("./routes/categoryRoutes")
-const paymentRoutes=require('./routes/paymentRoutes')
-const ExpressError = require("./middleware/ExpressError");
-const GlobalErrorHandler = require("./middleware/GlobalErrorHandler");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+const eventRoutes = require('./routes/eventRoutes')
+const attendeeRoutes = require('./routes/attendeeRoutes')
+const authRoutes = require('./routes/authRoutes')
+const ticketRoutes = require('./routes/ticketRoutes')
+const reviewRoutes = require('./routes/reviewRoutes')
+const categoryRoutes = require('./routes/categoryRoutes')
+const paymentRoutes = require('./routes/paymentRoutes')
+const ExpressError = require('./middleware/ExpressError')
+const GlobalErrorHandler = require('./middleware/GlobalErrorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL, {
-    });
-    console.log("Connected to Mongo succesfully");
+    mongoose.connect(process.env.MONGO_URL, {})
+    console.log('Connected to Mongo succesfully')
   } catch (err) {
-    console.log("Error while connecting to database");
+    console.log('Error while connecting to database')
   }
-};
-connectDB();
+}
+connectDB()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
   credentials: true,
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use("/api", eventRoutes);
-app.use("/api", attendeeRoutes);
-app.use("/api", authRoutes);
-app.use("/api", ticketRoutes);
-app.use("/api/event", categoryRoutes);
-app.use("/api/event", reviewRoutes);
-app.use('/api',paymentRoutes)
+app.use('/api', eventRoutes)
+app.use('/api', attendeeRoutes)
+app.use('/api', authRoutes)
+app.use('/api', ticketRoutes)
+app.use('/api/event', categoryRoutes)
+app.use('/api/event', reviewRoutes)
+app.use('/api', paymentRoutes)
 
-app.get('/',(req,res)=>{
-  res.send("Event Management")
+app.get('/api', (req, res) => {
+  res.send('Event Management')
+})
+app.all('*', (req, res, next) => {
+  try {
+    next(new ExpressError(404, false, 'Page not found'))
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.all("*", (req, res, next) => {
-  try {
-    throw new ExpressError(404, false, "Page not found");
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.use(GlobalErrorHandler);
+app.use(GlobalErrorHandler)
 
 app.listen(PORT, () => {
-  console.log("LISTENING TO THE PORT");
-});
+  console.log('LISTENING TO THE PORT')
+})
