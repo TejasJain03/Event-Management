7db62f6f9278e726f12a09121fe07f3200e0623b
unknown <tejaskjain2003@gmail.com>
2023-11-14 10:18:34 +0530

    Event Management

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ connectDB()
 const PORT = process.env.PORT || 5000
 
 const corsOptions = {
-  origin: 'https://event-management-client-alpha.vercel.app',
+  origin: 'https://event-management-three.vercel.app',
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
