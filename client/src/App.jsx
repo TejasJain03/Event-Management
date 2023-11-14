611220499f167adddeee9ba1cf4573e173730b5e
unknown <tejaskjain2003@gmail.com>
2023-11-14 10:13:22 +0530

    Event Management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,32 +12,29 @@ import UpdateEvent from "./components/eventPages/UpdateEvent";
 import CreateEvent from "./components/eventPages/CreateEvent";
 import UserEvents from "./components/userPages/UserEvents";
 import ManageUserEvent from "./components/userPages/ManageUserEvent";
-import AboutEvent from "./components/eventPages/AboutEvent"
+import AboutEvent from "./components/eventPages/AboutEvent";
 import BookTicket from "./components/eventPages/BookTicket";
 import PaymentConfirm from "./components/paymentPages/PaymentConfirm";
 import PaymentSuccess from "./components/paymentPages/PaymentSuccess";
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/showuserevents" element={<UserEvents />} />
-          <Route path="/aboutevent/:id" element={<AboutEvent />} />
-          <Route path="/manageevent" element={<ManageUserEvent/>}/>
-          <Route path="/booktickets" element={<BookTicket/>}/>
-          <Route path="/paymentconfirmation/:eventId" element={<PaymentConfirm/>}/>
-          <Route path="/paymentsuccess/:reference" element={<PaymentSuccess/>} />
-          <Route path="/updateevent/:eventId" element={<UpdateEvent />} />
-          <Route path="/createevent" element={<CreateEvent />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/showallevent" element={<AllEvent />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/showuserevents" element={<UserEvents />} />
+        <Route path="/aboutevent/:id" element={<AboutEvent />} />
+        <Route path="/manageevent" element={<ManageUserEvent />} />
+        <Route path="/booktickets" element={<BookTicket />} />
+        <Route path="/paymentconfirmation/:eventId" element={<PaymentConfirm />} />
+        <Route path="/paymentsuccess/:reference" element={<PaymentSuccess />} />
+        <Route path="/updateevent/:eventId" element={<UpdateEvent />} />
+        <Route path="/createevent" element={<CreateEvent />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/showallevent" element={<AllEvent />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
